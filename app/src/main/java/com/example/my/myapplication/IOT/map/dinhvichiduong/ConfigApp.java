049682a0695fbbea0f7350645ea58ebddcdf93d9
My <htmynguyen@gmail.com>
2018-04-04 00:33:31 +0700

    n

--- app/src/main/java/com/example/my/myapplication/IOT/map/dinhvichiduong/ConfigApp.java
+++
@@ -1,9 +0,0 @@
-package com.example.my.myapplication.IOT.map.dinhvichiduong;
-public class ConfigApp {
-
-    public static final String VITRIGPS = "vitrigps";
-
-    public static String getVITRIGPS() {
-        return VITRIGPS;
-    }
-}
--- app/src/main/java/com/example/my/myapplication/IOT/map/dinhvichiduong/DirectionFinderListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.my.myapplication.IOT.map.dinhvichiduong;
-
-import java.util.List;
-
-public interface DirectionFinderListener {
-    void onDirectionFinderStart();
-    void onDirectionFinderSuccess(List<Route> route);
-}
